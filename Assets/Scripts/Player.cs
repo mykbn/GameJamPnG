00429a1834e164c4fa0a1abfe358e144d9ccf059
Christopher Ian Chy <chrisian023@gmail.com>
2016-07-09 02:16:29 +0800

    *controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
+	public static Player Instance;
+
 	public GameObject goPlayer;
 	public Grid grid;
 	public Node currentNode;
 	public DIRECTIONS currentDirection;
 
 	void Awake(){
+		Instance = this;
 	}
 
 	// Use this for initialization
@@ -15,6 +19,8 @@ public class Player : MonoBehaviour {
 		SwipeControls.Swipe += FacePlayer;
 		currentNode = grid.NodeFromWorldPoint(goPlayer.transform.position);
 		goPlayer.transform.position = currentNode.worldPosition;
+		Debug.Log(goPlayer.transform.position);
+
 	}
 
 	// Update is called once per frame
@@ -38,6 +44,50 @@ public class Player : MonoBehaviour {
 		}
 		if(direction == DIRECTIONS.DOWN){
 			goPlayer.transform.Rotate(new Vector3(0f,0f,-90f));
+
+		}
+
+	}
+	public void MovePlayer(){
+		List<Node> neighbors = grid.GetNeighbours(currentNode);
+		if(currentDirection == DIRECTIONS.LEFT){
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
+			goPlayer.GetComponent<TweenPosition>().to = neighbors[1].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().PlayForward();
+//			goPlayer.transform.position = neighbors[1].worldPosition;
+			currentNode = neighbors[1];
+			return;
+
+		}
+		if(currentDirection == DIRECTIONS.RIGHT){
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
+			goPlayer.GetComponent<TweenPosition>().to = neighbors[6].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().PlayForward();
+//			goPlayer.transform.position = neighbors[6].worldPosition;
+			currentNode = neighbors[6];
+			return;
+
+		}
+		if(currentDirection == DIRECTIONS.UP){
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
+			goPlayer.GetComponent<TweenPosition>().to = neighbors[4].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().PlayForward();
+//			goPlayer.transform.position = neighbors[4].worldPosition;
+			currentNode = neighbors[4];
+			return;
+
+		}
+		if(currentDirection == DIRECTIONS.DOWN){
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
+			goPlayer.GetComponent<TweenPosition>().to = neighbors[3].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().PlayForward();
+//			goPlayer.transform.position = neighbors[3].worldPosition;
+			currentNode = neighbors[3];
+			return;
 
 		}
 
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInterface : MonoBehaviour {
+
+	public void OnClickTap(){
+		Debug.Log("TAPPED");
+		if(Player.Instance != null){
+			Player.Instance.MovePlayer();
+		}
+	}
+}
