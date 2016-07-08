621a7d6e015f140143b57c28f780c8a47926b105
Christopher Ian Chy <chrisian023@gmail.com>
2016-07-09 01:08:06 +0800

    +swipe controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,41 @@ public class Player : MonoBehaviour {
 	public GameObject goPlayer;
 	public Grid grid;
 	public Node currentNode;
+	public DIRECTIONS currentDirection;
 
 	void Awake(){
 	}
 
 	// Use this for initialization
 	void Start () {
+		SwipeControls.Swipe += FacePlayer;
 		currentNode = grid.NodeFromWorldPoint(goPlayer.transform.position);
 		goPlayer.transform.position = currentNode.worldPosition;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void FacePlayer(DIRECTIONS direction){
+		currentDirection = direction;
+		goPlayer.transform.rotation = Quaternion.identity;
+		if(direction == DIRECTIONS.LEFT){
+			goPlayer.transform.Rotate(new Vector3(0f,0f,180f));
+		}
+		if(direction == DIRECTIONS.RIGHT){
+			goPlayer.transform.Rotate(new Vector3(0f,0f,0f));
+
+		}
+		if(direction == DIRECTIONS.UP){
+			goPlayer.transform.Rotate(new Vector3(0f,0f,90f));
+
+		}
+		if(direction == DIRECTIONS.DOWN){
+			goPlayer.transform.Rotate(new Vector3(0f,0f,-90f));
+
+		}
+
 	}
 }
---
+++ Assets/Scripts/SwipeControls.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeControls : MonoBehaviour {
+
+	public delegate void OnSwipe(DIRECTIONS direction);
+	public static OnSwipe Swipe;
+
+	public float minSwipeDistY;
+	
+	public float minSwipeDistX;
+	
+	private Vector2 startPos;
+	
+	void Update()
+	{
+		if(Input.GetKey("up")){
+			SwipeControl(DIRECTIONS.UP);
+
+		}
+		if(Input.GetKey("down")){
+			SwipeControl(DIRECTIONS.DOWN);
+
+		}
+		if(Input.GetKey("right")){
+			SwipeControl(DIRECTIONS.RIGHT);
+
+		}
+		if(Input.GetKey("left")){
+			SwipeControl(DIRECTIONS.LEFT);
+
+		}
+
+		//#if UNITY_ANDROID
+		if (Input.touchCount > 0) 
+			
+		{
+			
+			Touch touch = Input.touches[0];
+			
+			switch (touch.phase){
+				
+			case TouchPhase.Began:
+				startPos = touch.position;
+				break;
+			case TouchPhase.Ended:
+				float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+				if (swipeDistVertical > minSwipeDistY) 
+				{
+					float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+					if (swipeValue > 0){//up
+						SwipeControl(DIRECTIONS.UP);
+					}
+												
+					else if (swipeValue < 0){//down
+						SwipeControl(DIRECTIONS.DOWN);
+					}
+								
+				}
+
+				float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+				if (swipeDistHorizontal > minSwipeDistX) 
+					
+				{
+					float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+					
+					if (swipeValue > 0){//right
+						SwipeControl(DIRECTIONS.RIGHT);
+					}
+						
+					else if (swipeValue < 0){//left
+						SwipeControl(DIRECTIONS.LEFT);
+					}
+							
+				}
+				break;
+			}
+		}
+	}
+
+	public void SwipeControl(DIRECTIONS direction){
+		if(Swipe != null){
+			Swipe(direction);
+		}
+	}
+}
