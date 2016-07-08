9705dc664fa89c4c4cbe274a1a6295580913972d
Mykel Abania <mykel.abania@loudpandagames.com>
2016-07-09 02:47:54 +0800

    +Camera follow and light effect

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField] private GameObject goPlayer;
+
+	Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+//		playerTransform = goPlayer.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(goPlayer.transform.position.x, goPlayer.transform.position.y, -10);
+	}
+}
