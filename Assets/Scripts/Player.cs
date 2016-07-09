4e156b82e3d641c16c679703e5d81875ff73823b
Christopher Ian Chy <chrisian023@gmail.com>
2016-07-09 15:15:58 +0800

    *plyaer movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,13 @@ public class Player : MonoBehaviour {
 	public static Player Instance;
 
 	public GameObject goPlayer;
+	public Rigidbody rgdPlayer;
 	public Grid grid;
 	public Node currentNode;
 	public DIRECTIONS currentDirection;
+
+	public bool isMoving;
+	public float speed;
 
 	void Awake(){
 		Instance = this;
@@ -16,19 +20,27 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		SwipeControls.Swipe += FacePlayer;
-		currentNode = grid.NodeFromWorldPoint(goPlayer.transform.position);
-		goPlayer.transform.position = currentNode.worldPosition;
-		Debug.Log(goPlayer.transform.position);
-
+		SwipeControls.Swipe += PlayerDirection;
+//		currentNode = grid.NodeFromWorldPoint(goPlayer.transform.position);
+//		goPlayer.transform.position = currentNode.worldPosition;
+//		Debug.Log(goPlayer.transform.position);
+		MovePlayer(currentDirection);
 	}
 
 	// Update is called once per frame
 	void Update () {
 	
+		if(isMoving){
+//			MovePlayer(currentDirection);
+		}
 	}
-
+	public void PlayerDirection(DIRECTIONS direction){
+		FacePlayer(direction);
+	}
 	public void FacePlayer(DIRECTIONS direction){
+//		if(!isMoving && direction == currentDirection){
+//			return;
+//		}
 		currentDirection = direction;
 		goPlayer.transform.rotation = Quaternion.identity;
 		if(direction == DIRECTIONS.LEFT){
@@ -46,14 +58,34 @@ public class Player : MonoBehaviour {
 			goPlayer.transform.Rotate(new Vector3(0f,0f,-90f));
 
 		}
+//		isMoving = true;
+		MovePlayer(direction);
+	}
+	public void MovePlayer(DIRECTIONS direction){
+		rgdPlayer.velocity = Vector3.zero;
+		if(direction == DIRECTIONS.LEFT){
+			rgdPlayer.AddForce (Vector3.left * speed, ForceMode.Force);
+		}
+		if(direction == DIRECTIONS.RIGHT){
+			rgdPlayer.AddForce (Vector3.right * speed, ForceMode.Force);
 
+		}
+		if(direction == DIRECTIONS.UP){
+			rgdPlayer.AddForce (Vector3.up * speed, ForceMode.Force);
+
+		}
+		if(direction == DIRECTIONS.DOWN){
+			rgdPlayer.AddForce (Vector3.down * speed, ForceMode.Force);
+
+		}
 	}
 	public void MovePlayer(){
 		List<Node> neighbors = grid.GetNeighbours(currentNode);
 		if(currentDirection == DIRECTIONS.LEFT){
-			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
 			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
 			goPlayer.GetComponent<TweenPosition>().to = neighbors[1].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+
 			goPlayer.GetComponent<TweenPosition>().PlayForward();
 //			goPlayer.transform.position = neighbors[1].worldPosition;
 			currentNode = neighbors[1];
@@ -61,9 +93,10 @@ public class Player : MonoBehaviour {
 
 		}
 		if(currentDirection == DIRECTIONS.RIGHT){
-			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
 			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
 			goPlayer.GetComponent<TweenPosition>().to = neighbors[6].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+
 			goPlayer.GetComponent<TweenPosition>().PlayForward();
 //			goPlayer.transform.position = neighbors[6].worldPosition;
 			currentNode = neighbors[6];
@@ -71,9 +104,10 @@ public class Player : MonoBehaviour {
 
 		}
 		if(currentDirection == DIRECTIONS.UP){
-			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
 			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
 			goPlayer.GetComponent<TweenPosition>().to = neighbors[4].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+
 			goPlayer.GetComponent<TweenPosition>().PlayForward();
 //			goPlayer.transform.position = neighbors[4].worldPosition;
 			currentNode = neighbors[4];
@@ -81,9 +115,10 @@ public class Player : MonoBehaviour {
 
 		}
 		if(currentDirection == DIRECTIONS.DOWN){
-			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
 			goPlayer.GetComponent<TweenPosition>().from = currentNode.worldPosition;
 			goPlayer.GetComponent<TweenPosition>().to = neighbors[3].worldPosition;
+			goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+
 			goPlayer.GetComponent<TweenPosition>().PlayForward();
 //			goPlayer.transform.position = neighbors[3].worldPosition;
 			currentNode = neighbors[3];
@@ -91,5 +126,20 @@ public class Player : MonoBehaviour {
 
 		}
 
+	}
+	public void StopMoving(){
+		isMoving = false;
+		Debug.Log(goPlayer.transform.position);
+//		currentNode = grid.NodeFromWorldPoint(goPlayer.transform.position);
+//		goPlayer.GetComponent<TweenPosition>().from = goPlayer.transform.position;
+//		goPlayer.GetComponent<TweenPosition>().to = currentNode.worldPosition;
+//		goPlayer.GetComponent<TweenPosition>().ResetToBeginning();
+//		goPlayer.GetComponent<TweenPosition>().PlayForward();
+	}
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "wall"){
+			Debug.Log("WALL");
+//			StopMoving();
+		}
 	}
 }
